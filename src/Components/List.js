3ecd87f3b3094da2daada8fc1a73b050287c052c
willsinn <30275717+willsinn@users.noreply.github.com>
2018-06-19 16:06:13 -0400

    Created Components

---
+++ src/Components/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+
+  render() {
+    return(
+      <div className="List">
+        List Component
+      </div>
+    );
+  }
+}
+
+export default List;
