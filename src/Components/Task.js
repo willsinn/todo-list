72fc4030a4e6f73a6eaf28fc2e9e5569736154c4
willsinn <30275717+willsinn@users.noreply.github.com>
2018-06-20 13:57:14 -0400

    editing

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -2,17 +2,45 @@ import React, { Component } from 'react';
 
 class Task extends Component {
 
+constructor(props){
+  super(props);
+  this.state= { editing:false }
+}
+
+  handleEditing(event) {
+    this.setState({ editing:true })
+  }
+
+  handleEditingDone(event) {
+    if (event.keyCode === 13)
+      this.setState({editing: false});
+  }
 
 
   render() {
+      var todo = this.props.todo;
+
+      var viewStyle = {};
+      var editStyle = {};
+
+      if (this.state.editing) {
+        viewStyle.display = 'none';
+      } else {
+        editStyle.display = 'none';
+      }
     return(
-      <div className="Task Component">
+      <li className="Task Component">
         <span style={{ textDecoration: this.props.todo.done ? 'line-through': 'none' }}>
           {this.props.todo.value}
         </span>
         <button onClick={ () =>
           this.props.handleCompleteToggle(this.props.index)}> {this.props.todo.done ? 'Undo': 'Complete'} </button>
-      </div>
+        <input type="text"
+              onKeyDown={this.handleEditingDone.bind(this)}
+              style= {editStyle}
+              value= {todo.inputValue} />
+      </li>
+
     );
   }
 }
